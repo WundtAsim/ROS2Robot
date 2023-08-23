fix the quaternion to mat function: (w,x,y,z)&'+'

data/calib/check.py
@@ -1,5 +1,3 @@
-import grp
-from hmac import new
 import numpy as np
 import cv2
 
@@ -30,8 +28,8 @@ R_cam2grp, t_cam2grp = cv2.calibrateHandEye(grp2base_r, grp2base_t,
 print('cam2grp_t',t_cam2grp)
 
 for i in range(25):
-    a1 = (grp2base_r[i] * np.array([0,0,0]).reshape(3,-1))[0].reshape(3,-1) + grp2base_t[i]*100
-    a2 = (grp2base_r[i] * np.array([100,0,0]).reshape(3,-1))[0].reshape(3,-1) + grp2base_t[i]*100
+    a1 = (grp2base_r[i] @ np.array([0,0,0]).reshape(3,-1)).reshape(3,-1) + grp2base_t[i]*100
+    a2 = (grp2base_r[i] @ np.array([100,0,0]).reshape(3,-1)).reshape(3,-1) + grp2base_t[i]*100
     print(distance(a1,a2))
 
 

src/vision_py/vision_py/calib_eyeinhand.py
@@ -113,14 +113,17 @@ class Calibrator_eyeinhand(Calc_tgt2cam):
             return None, None
         
     def quaternion_to_rotation_matrix(self, q):  # x, y ,z ,w
-        x = q[0]
-        y = q[1]
-        z = q[2]
-        w = q[3]
+        '''
+        same as transforms3d.quaternions.quat2mat: use parameter (w,x,y,z)
+        '''
+        w = q[0]
+        x = q[1]
+        y = q[2]
+        z = q[3]
         rot_matrix = np.array(
             [[1.0 - 2*(y**2 + z**2),    2*(x*y - w*z),          2*(x*z + w*y)],
             [2*(x*y + w*z),             1.0 - 2*(x**2 + z**2),  2*(y*z - w*x)],
-            [2*(x*z - w*y),             2*(y*z - w*x),          1.0 - 2*(x**2 + y**2)]],
+            [2*(x*z - w*y),             2*(y*z + w*x),          1.0 - 2*(x**2 + y**2)]],
             dtype=q.dtype)
         return rot_matrix
 